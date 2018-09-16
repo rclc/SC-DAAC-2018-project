ccc7300b3a7c973dbc17a9da4492d9c030589106
gwuece <cchiang@stthomas.edu>
2018-09-16 07:26:22 -0500

    First public commit

---
+++ install-docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SET UP THE REPOSITORY
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+#install docker ce
+sudo apt-get update
+sudo apt-get install docker-ce
+#verify that Docker CE is installed correctly
+sudo docker run hello-world
